5660770474f616f6b6243b9cc51b300cf2eb22db
Aleena <aleenabennyskpm@gmail.com>
2022-01-31 23:57:35 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,11 @@
+first_name=input("What is your name? ")
+print("Hello" +first_name)
+birth_year=input("Enter your birth year ")
+age=2022-int(birth_year)
+print("Her age is "+str(age))
+status=input("Are you a new patient? ")
+if  status== "yes":
+ print( first_name +" is a new patient" )
+else:
+ print( first_name +" is not a new patient" )
+
